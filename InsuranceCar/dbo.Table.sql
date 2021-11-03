4d51fd88a4eb3e3e5a13087930392d71c3d046e8
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-11-02 22:37:10 -0400

    Page 514

---
+++ InsuranceCar/dbo.Table.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Insurees]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (0,1),
+	[FirstName] NVARCHAR(50) NOT NULL,
+	[LastName] NVARCHAR(50) NOT NULL,
+	[EmailAddress] NVARCHAR(50) NOT NULL,
+	[DateOfBirth] DATETIME NOT NULL,
+	[CarYear] INT NOT NULL,
+	[CarMake] NVARCHAR(50) NOT NULL,
+	[CarModel] NVARCHAR(50) NOT NULL,
+	[DUI] Varchar(16)  NULL,
+	[SpeedingTickets] INT NOT NULL,
+	[CoverageType] varchar(16)  NULL,
+	[Quote] MONEY NOT NULL
+);
